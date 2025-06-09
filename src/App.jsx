722b03e868a142d3ac0882c98cfee0c4a6d83be4
AkashSaha01 <akashsaha661@gmail.com>
2025-06-10 00:41:07 +0600

    button updated

--- src/App.jsx
+++ src/App.jsx
@@ -4,11 +4,13 @@ function App() {
       <header>
         <div className="w-[90%] mx-auto flex justify-between items-center p-4">
           <div>
-            <h1 className="text-xl font-bold pl-6 text-blue-800">Portfolio</h1>
+            <h1 className="text-2xl font-bold font-roboto pl-6 text-blue-800">
+              Portfolio
+            </h1>
           </div>
 
-          <nav>
-            <ul className="flex gap-4 list-none">
+          <nav className="py-2">
+            <ul className="flex gap-6 list-none">
               <li>
                 <a href="#about">About</a>
               </li>
@@ -25,10 +27,11 @@ function App() {
           </nav>
 
           <div className="flex gap-2">
-            <button className="px-4 py-2 bg-blue-500 text-white rounded">
+            <button className="px-4 py-2 bg-white text-black rounded border border-gray-300">
               Resume
             </button>
-            <button className="px-4 py-2 bg-green-500 text-white rounded">
+
+            <button className="px-4 py-2 bg-white text-black rounded border border-gray-300">
               Hire me
             </button>
           </div>
